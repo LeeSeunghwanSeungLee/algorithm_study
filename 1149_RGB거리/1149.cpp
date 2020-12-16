dbdb71e41ece3986e5c207eb54fe230040086151
SeunghwanLee <lshn1007@hanyang.ac.kr>
2020-12-16 21:16:48 +0900

    20-12-16

--- 1149_RGB거리/1149.cpp
+++ 1149_RGB거리/1149.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
-
+#include <algorithm>
 
 
 using namespace std;
 //전역변수
 int** template_arr;
-
+long long** dp_arr;
 int main(void){
     int size;
     cin >> size;
-
-
+    
+    
     template_arr = new int*[size]; // 동적으로 배열 생성
+    dp_arr = new long long*[size];
     for(int i = 0; i < size; i++){
         template_arr[i] = new int[3];
+        dp_arr[i] = new long long[3];
     }
 
     for(int i = 0; i < size; i++){ // 변수 담기
@@ -22,15 +24,31 @@ int main(void){
         }
     }
 
+    for(int i = 0; i < 3; i++){
+        dp_arr[0][i] = (long long)template_arr[0][i]; // 형변환
+    }
+
     //연산
-    int dp[size] = {0,}; // 동적 프로그래밍
+    for(int i = 1; i < size; i++){
+        for(int j = 0; j < 3; j++){
+            long long min = 9223372036854775807;
+            for(int k = 0; k < 3; k++){
+                if(k == j) continue;
+                if(dp_arr[i-1][k] < min) min = dp_arr[i-1][k];
+            }
+            dp_arr[i][j] = template_arr[i][j] + min;
+        }
+    }
+    
+    long long ans = min(min(dp_arr[size-1][0], dp_arr[size-1][1]), dp_arr[size - 1][2]);
+    cout << ans;
     
 
 
 
 
 
-
-
+    delete[] template_arr; // 질문 더블포인터는 동적 메모리 해제 어떻게 하는지 확인하기!
+    delete[] dp_arr;
     return 0;
 }
