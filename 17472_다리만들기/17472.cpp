169c6a1fc664159b10ce97dafb5c7b5ccf207171
lshn1007 <lshn1007@hanyang.ac.kr>
2021-02-16 00:35:39 +0900

    21-02-16

--- 17472_다리만들기/17472.cpp
+++ 17472_다리만들기/17472.cpp
@@ -1,13 +1,64 @@
 #include <iostream>
+#include <queue>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
 int N, M;
 int GRAPH[10][10];
-bool CONTINENT[7][7]; // 대륙간 연결성을 확인하기 위한 장표
+bool CHECK[10][10];
 int CONTINENT_DIST[7][7];
 int ANS(0);
+int FLAG(1);
+int d_r[] = {-1, 0, 1, 0};
+int d_c[] = {0, 1, 0, -1};
+vector<bool> FLAG_CHECK;
+vector<int> CALC_LIST;
+
+void BFS(int row, int col, int num){
+    CHECK[row][col] = true;
+    queue<pair<int, int>> tmp;
+    tmp.push(make_pair(row, col));
+    while(!tmp.empty()){
+        int now_row = tmp.front().first;
+        int now_col = tmp.front().second;
+        GRAPH[now_row][now_col] = num;
+        tmp.pop();
+        for(int i = 0; i < 4; i++){
+            int next_row = now_row + d_r[i];
+            int next_col = now_col + d_c[i];
+            if(next_row < 0 || next_row >= N || next_col < 0 || next_col >= M) continue;
+            if(CHECK[next_row][next_col] == true) continue;
+            if(GRAPH[next_row][next_col] != 1) continue;
+            tmp.push(make_pair(next_row, next_col));
+            CHECK[next_row][next_col] = true;
+        }
+    }
+}
+
+void DFS(int depth){
+    if(depth >= FLAG - 1){
+        int final;
+        for(int i = 1; i <= FLAG; i++){
+            if(FLAG_CHECK[i] == false) final = i;
+        }
+        for(int j = 1; j <= FLAG; j++){
+            int final_2 = j;
+        }
+        //여기서부터 연산
+        return;
+    }
+    for(int i = 1; i <= FLAG; i++){
+        if(FLAG_CHECK[i] == false){
+            FLAG_CHECK[i] = true;
+            CALC_LIST.push_back(i);
+            DFS(depth + 1);
+            FLAG_CHECK[i] = false;
+            CALC_LIST.pop_back();
+        }
+    }
+}
 
 void init(){
     ios_base::sync_with_stdio(false);
@@ -18,34 +69,75 @@ void init(){
     for(int i = 0; i < N; i++){
         for(int j = 0; j < M; j++){
             cin >> GRAPH[i][j];
-            if(GRAPH[i][j] == 1){
-                //상하 좌우의 영역을 체크한다
-                //if 0이아닌 녀석이 있으면 그 값을 GRAPH[i][j] 에 넣는다
-                //else GRAPH[i][j] = ++area; 
+        }
+    }
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            if(GRAPH[i][j] == 1 && CHECK[i][j] == false){
+                BFS(i, j, FLAG);
+                FLAG++;
             }
         }
     }
+    FLAG--;
+    for(int i = 0; i <= FLAG; i++){
+        FLAG_CHECK.push_back(false);
+    }
+    memset(CONTINENT_DIST, -1, sizeof(CONTINENT_DIST));
 }
 int main(void){
     init();
+   
     for(int row = 0; row < N; row++){ // 가로에서 확인
-        //from to 선언 (-1)
-        //이거를 queue에 pair로 담는다
+        int from(-1), to(-1);
         for(int col = 0; col < M; col++){
-            //만일 0 이 아닌 녀석이 나오면 from To 순서대로 저장
+            if(GRAPH[row][col] != 0){
+                //if 둘다 -1 이면 from 에 저장 컨티뉴
+                //else if from만 -1 이 아니면 to 에 저장후 거리를 계산한다
+                    //if 거리가 2이상이면
+                        //CONTINENT[from][to] 값이 -1 이면 그대로 저장하고
+                        //아니면 더 작을때만 저장한다
+                if(from == -1 && to == -1){
+                    from = GRAPH[row][col];
+                    continue;
+                }
+                else if (from != -1){
+                    to = GRAPH[row][col];
+                    if(to - from - 1 <= 1){
+                        from = -1, to = -1;
+                        continue;
+                    }
+                    else{
+                        if(CONTINENT_DIST[row][col] == -1){
+                            CONTINENT_DIST[from][to] = to - from - 1;
+                            CONTINENT_DIST[to][from] = to - from - 1;
+                        }
+                        else{
+                            if(CONTINUE_DIST[from][to] <= to - from - 1) continue;
+                            CONTINENT_DIST[from][to] = to - from - 1;
+                            CONTINENT_DIST[to][from] = to - from - 1;
+                        }
+                    }
+                }
+            }
         }
-        //while(!.queue.empty())
-            //만일 거리가 1이하면 컨티뉴
-            //아니면 
-                //CONTINENT[from][to] 가 false 면 true 로 변경 후 기존의 true엿던 녀석들을통해 전부 다리를 놓아준다
-                //true면 CONTINENT_DIST에서 더 짧은 녀석으로 교체한다
-                //이후 ANS를 수정한다
     }
 
     for(int col = 0; col < M; col++){ // 세로에서 확인
-
+        int from(-1), to(-1);
+        for(int row = 0; row < N; row++){
+            if(GRAPH[row][col] != 0){
+                //if 둘다 -1 이면 from 에 저장 컨티뉴
+                //else if from만 -1 이 아니면 to 에 저장후 거리를 계산한다
+                    //if 거리가 2이상이면
+                        //CONTINENT[from][to] 값이 -1 이면 그대로 저장하고
+                        //아니면 더 작을때만 저장한다
+            }
+        }
     }
 
+    //DFS로 최단거리를 찾는다 1 ~ FLAG 에 다가 1개만 더추가
+    
 
     return 0;
 }
