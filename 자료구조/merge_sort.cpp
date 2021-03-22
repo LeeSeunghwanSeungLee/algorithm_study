706388c15de418ec5bdfd2cb093bebdb2de05a20
lshn1007 <lshn1007@hanyang.ac.kr>
2021-03-22 23:20:41 +0900

    21-03-22

--- 자료구조/merge_sort.cpp
+++ 자료구조/merge_sort.cpp
@@ -29,3 +29,32 @@ void m_sort(int l, int r, int* arr){
 			arr[u] = sort[u];
     }
 }
+
+char* tmp_sort;
+
+void m_sort_char(int l, int r, char* sort_char){
+    if(l < r){
+        int mid = (l + r) >> 1;
+        int i = l, j = mid, k = l, u;
+        m_sort(l, mid, sort_char);
+        m_sort(mid + 1, r, sort_char);
+    }
+
+    while(i <= mid && j <= r){
+        if(sort_char[i] <= sort_char[j]){
+            tmp_sort[k++] = sort_char[i++];
+        }
+        else{
+            tmp_sort[k++] = sort_char[j++];
+        }
+    }
+    if(i > mid){ // 오른쪽이 남아있는 경우
+        for(u = j; u <= r; u++){
+
+        }
+    }
+    else{ // 왼쪽이 남아있는 경우
+        for(u = i; i <= mid; i++)
+    }
+
+}
