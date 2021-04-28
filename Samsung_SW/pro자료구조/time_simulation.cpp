27e69244d1559fc6dc6cc461ee4c2643cc475be0
lshn1007 <lshn1007@hanyang.ac.kr>
2021-04-28 23:01:31 +0900

    21-04-28

--- Samsung_SW/pro자료구조/time_simulation.cpp
+++ Samsung_SW/pro자료구조/time_simulation.cpp
@@ -1,93 +1,77 @@
-#define MAX_BUS_NUM 70001
-#define MAX_TIME 400001
-#define NULL 0
-
-typedef struct BUS{
+#define MAX_BUS 71000
+ 
+typedef struct Bus {
+    int endtime;
     int idx;
-    int end_time;
     int num;
-    BUS* next;
-}bus;
+}Bus;
+ 
+Bus busbusbusbus[MAX_BUS];
+int cnt;
+ 
 
-bus ALLOC[MAX_BUS_NUM];
-int alloc_id = 0;
-
-bus* TIME_LINE[MAX_TIME];
-
-bus* bus_alloc(){
-    return &ALLOC[alloc_id++];
+void init() {
+    cnt = 0;
 }
-
-void init(){
-    alloc_id = 0;
-    for(register int i = 0; i < MAX_TIME ; i++){
-        TIME_LINE[i] = NULL;
-    }
+ 
+void start(int time, int idx, int traveltime, int num) {
+    busbusbusbus[cnt++] = { time + traveltime, idx, num }; 
 }
-
-void start(int time, int idx , int traveltime, int num){
-    bus* n_bus = bus_alloc();
-
-    register int start = time;
-    int end = start + traveltime;
-
-    n_bus->idx = idx;
-    n_bus->end_time = end;
-    n_bus->num = num;
-    n_bus->next = NULL;
-
-    for (start; start < end; start++){
-        if(TIME_LINE[start] == NULL){ //만일 처음이면
-            TIME_LINE[start] = n_bus;
+ 
+int getSum(int time) {
+    register int sum = 0;
+    for(register int i =0; i<cnt;)
+    {
+        if (busbusbusbus[i].endtime <= time) busbusbusbus[i] = busbusbusbus[--cnt];
+        else
+        {
+            sum += busbusbusbus[i].num;
+            i++;
         }
-        else{ // 처음이 아니면
-            bus* now_bus = TIME_LINE[start];
-            while(now_bus->next != NULL){
-                now_bus = now_bus -> next;
+ 
+    }
+    return sum;
+}
+ 
+int getLast(int time) {
+    register int idx = 0, last = 0;
+    for (register int i = 0; i < cnt;)
+    {
+        if (busbusbusbus[i].endtime <= time) busbusbusbus[i] = busbusbusbus[--cnt];
+        else
+        {
+            if(last < busbusbusbus[i].endtime)               
+            {
+                last = busbusbusbus[i].endtime;
+                idx = busbusbusbus[i].idx;
             }
-            now_bus->next = n_bus;
+            else if ((last == busbusbusbus[i].endtime) && (idx > busbusbusbus[i].idx)) idx = busbusbusbus[i].idx;
+ 
+            i++;
         }
+ 
     }
+    return idx;
 }
-
-int getSum(int time){
-    int tot = 0;
-    bus* now_bus = TIME_LINE[time];
-    if(now_bus == NULL) return tot;
-
-    while(now_bus->next != NULL){
-        tot += now_bus -> num;
-        now_bus = now_bus -> next;
+ 
+int getMax(int time) {
+    register int idx = 0, max = 0;
+    for (register int i = 0; i < cnt;)
+    {
+        if (busbusbusbus[i].endtime <= time) busbusbusbus[i] = busbusbusbus[--cnt];
+        else
+        {
+            if (max < busbusbusbus[i].num)
+            {
+                max = busbusbusbus[i].num;
+                idx = busbusbusbus[i].idx;
+ 
+            }
+            else if((max == busbusbusbus[i].num) && (idx > busbusbusbus[i].idx)) idx = busbusbusbus[i].idx;
+ 
+            i++;
+        }
+ 
     }
-
-    tot += now_bus -> num;
-    return tot;
+    return idx;
 }
-
-int getLast(int time){
-    bus* now_bus = TIME_LINE[time];
-    int end = now_bus -> end_time;
-    while(now_bus->next != NULL){
-        now_bus = now_bus -> next;
-        if(end < now_bus -> end_time) end = now_bus -> end_time;
-    }
-    if(end > now_bus -> end_time) end = now_bus -> end_time;
-    return end;
-}
-
-int getMax(int time){
-    bus* now_bus = TIME_LINE[time];
-    int num = now_bus -> num;
-    while(now_bus->next != NULL){
-        now_bus = now_bus -> next;
-        if(num < now_bus -> num) num = now_bus -> num;
-    }
-    if(num < now_bus -> num) num = now_bus -> num;
-    return num;
-}
-
-
-// 1st try => time error!
-
-
-
