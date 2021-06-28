188927973a7371c008c4d5ca09ddcb652ffff44f
lshn1007 <lshn1007@hanyang.ac.kr>
2021-06-28 20:39:00 +0900

    21-06-28 : Trie Structure

---
+++ SW_samsung/Pro_Exercise008/trie.cpp
@@ -0,0 +1,15 @@
+void init_trie(){
+
+}
+void Tries_insert(char key[101]){
+
+}
+bool Tries_search(char key[101]){
+
+}
+int Tries_CountofkeyWithPrefix(char key[101]){
+
+}
+void Tries_deleteKey(char key[101]){
+
+}s
