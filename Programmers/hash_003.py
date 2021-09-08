c8a254a9afcc8274f50508c7792902d11a9ce037
lshn1007 <lshn1007@hanyang.ac.kr>
2021-09-08 17:59:43 +0900

    21-09-08 : (3)
    위장

---
+++ Programmers/hash_003.py
@@ -0,0 +1,42 @@
+# # 1. dictionary 형태로 type : value 로 선언한다 O(n)
+# # 2. iterator로 순회하면서 1 * (value + 1) 로 값을 돌린다
+# # 3. 마지막에 1을 제거해준다(아무것도 안 입을 수는 없으니까)
+
+# '''
+#     %%key points%%
+# 1. 2 dimension array to dictionary (type : num)
+# 2. logical idea
+# '''
+
+
+# def solution(clothes):
+#     answer = 0
+#     return answer
+
+# def solution(clothes):
+#     answer = {}
+#     for i in clothes:
+#         if i[1] in answer: answer[i[1]] += 1
+#         else: answer[i[1]] = 1
+
+#     cnt = 1
+#     for i in answer.values():
+#         cnt *= (i+1)
+#     return cnt - 1
+
+
+TEST = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+## module 이용
+from collections import Counter
+from functools import reduce
+
+
+
+def solution(clothes):
+    
+    cnt = Counter([kind for name, kind in clothes])
+    print(cnt.values())
+    answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+    return answer
+    
+print(solution(TEST))
