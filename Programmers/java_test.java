4b9e3fa55a7a34acb0024abbf45438b22a5be6db
lshn1007 <lshn1007@hanyang.ac.kr>
2021-09-27 23:08:08 +0900

    21-09-27 : Intall Java SE 16.02

---
+++ Programmers/java_test.java
@@ -0,0 +1,8 @@
+public class java_test {
+    public static void main(String[] arg){
+        System.out.println("Hello Java");
+    }
+}
+
+// Java Setting done
+// 1. Do not need to Install Iclipse & it's ligther
